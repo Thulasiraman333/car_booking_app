c5556986e19b10d46a4c1112338d7101459ef565
Thulasiraman333 <36729169+Thulasiraman333@users.noreply.github.com>
2025-10-03 09:31:14 +0530

    Car Rental Initial setup components

---
+++ CAR_RENTAL_APP/src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard', component: DashboardComponent,
+      },
+      {
+        path: 'booking', component: BookingsComponent
+      },
+      {
+        path: 'vehicles', component: VehiclesComponent
+      }
+    ]
+  }
+];
---
+++ CAR_RENTAL_APP/src/app/pages/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  loginObj: any = {
+    username: '',
+    password: ''
+  }
+  router = inject(Router);
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onLogin() {
+    if (this.loginObj.username == 'admin' && this.loginObj.password == 'admin') {
+      this.router.navigate(['/dashboard'])
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
+}
